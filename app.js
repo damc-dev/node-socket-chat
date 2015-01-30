95d4793818786625590ff7eed80271569fb9140c
David McElligott <damc.dev@gmail.com>
2015-01-30 10:05:30 -0600

    Added User names and test case

--- app.js
+++ app.js
@@ -6,24 +6,29 @@ app.get('/', function(req, res){
 	res.sendFile(__dirname + '/static/index.html');
 });
 
-var users = [];
+var clients = [];
 
-io.on('connection',function(socket) {
-	users.push(socket);
+var handleClient = function(socket) {
 	console.log('a user connected');
 	socket.on('user:connected', function(user) {
+        clients[socket.id] = user;
 		console.log(user.name + " has connected");
-		io.emit('user:joined', user.name + " has joined.");
-	})
+		io.emit('user:joined', {'name': user.name});
+	});
 
-	socket.on('chat message', function(msg) {
-		console.log('message: ' + msg);
-		io.emit('chat message', msg);
+	socket.on('message', function(message) {
+        var envelope = {};
+        envelope.userName = clients[socket.id].name;
+        envelope.msg = message;
+		console.log( envelope.userName + ': ' + envelope.msg);
+		io.emit('message', envelope);
 	});
 	socket.on('disconnect', function(){
 		console.log('user disconnected');
 	});
-});
+}
+
+io.on('connection', handleClient);
 
 http.listen(3000, function(){
 	console.log('listining on *:3000');
--- test/app-test.js
+++ test/app-test.js
@@ -12,36 +12,93 @@ var chatUser1 = {'name':'Tom'};
 var chatUser2 = {'name':'Sally'};
 var chatUser3 = {'name':'Dana'};
 
-describe("Chat Server",function(){
+describe("Chat Server",function() {
+    it('should broadcast new user to all users', function(done) {
 
-  it('should broadcast new user to all users', function(done) {
+        var client1 = io.connect(socketURL, options);
 
-    var client1 = io.connect(socketURL, options);
+        client1.on('connect', function(data){
+          client1.emit('user:connected', chatUser1);
 
-    client1.on('connect', function(data){
-      client1.emit('user:connected', chatUser1);
+          var client2 = io.connect(socketURL, options);
+          client2.on('connect',function(data) {
+            client2.emit('user:connected', chatUser2);
+          });
 
-      var client2 = io.connect(socketURL, options);
-      client2.on('connect',function(data) {
-        client2.emit('user:connected', chatUser2);
-      });
+          client2.on('user:joined', function(user) {
+            user.name.should.equal(chatUser2.name);
+            client2.disconnect();
+          });
+        });
 
-      client2.on('user:joined', function(user) {
-        user.should.equal(chatUser2.name + " has joined.");
-      });
+        var numUsers = 0;
+        client1.on('user:joined', function(user) {
+          numUsers++;
+
+          if(numUsers ==2) {
+            user.name.should.equal(chatUser2.name);
+            client1.disconnect();
+            done();
+          }
+        });
     });
+    
+    it('message envelope should contain user name', function(done) {
+        var client1, client2;
+        var message = 'Hello World!';
+        client1 = io.connect(socketURL, options);
+        client1.on('connect', function(data) {
+            client1.emit('user:connected', chatUser1);
 
-    var numUsers = 0;
-    client1.on('user:joined', function(user) {
-      numUsers++;
-
-      if(numUsers ==2) {
-        user.should.equal(chatUser2.name + " has joined.");
-        client1.disconnect();
+            client1.on('message', function(envelope) {
+                envelope.userName.should.equal(chatUser2.name);
+                client1.disconnect();
+            });
+            
+            client2 = io.connect(socketURL, options);
+            client2.on('connect', function(data) {
+                client2.emit('user:connected', chatUser2);
+                client2.send(message);    
+            });
+        });
         done();
-      }
     });
 
-  });
+    it('should be able to broadcast messages', function(done) {
+        var client1, client2, client3;
+        var message = 'Hello World!';
+        var messages = 0;
 
+        var checkMessage = function(client) {
+            client.on('message', function(envelope){
+                envelope.msg.should.equal(message);
+                client.disconnect();
+                messages++;
+                if(messages == 3) {
+                    done();
+                }
+            });
+        };
+        client1 = io.connect(socketURL, options);
+        checkMessage(client1);
+
+        client1.on('connect', function(data) {
+            client1.emit('user:connected', chatUser1);
+
+            client2 = io.connect(socketURL, options);
+            checkMessage(client2);
+
+            client2.on('connect', function(data) {
+                client2.emit('user:connected', chatUser2);
+
+                client3 = io.connect(socketURL, options);
+                checkMessage(client3);
+
+                client3.on('connect', function(data) {
+                    client3.emit('user:connected', chatUser3);
+                    client2.send(message);
+                });
+            });
+        });
+    });
 });
